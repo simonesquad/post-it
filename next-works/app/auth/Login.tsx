938e853753fab30341cf71795c0d109292b316c0
Simone Ballard <simone.a.ballard@gmail.com>
2023-03-10 16:55:51 -0600

    checking back on the roboto functionality later on

---
+++ next-works/app/auth/Login.tsx
@@ -0,0 +1,15 @@
+"use client"
+
+import { signIn } from "next-auth/react"
+
+export default function Login() {
+  return (
+    <li>
+      <button 
+        onClick={() => signIn()}
+        className="text-sm bg-gray-700 text-white py-2 px-6 rounded-xl disabled:opacity-25"
+      
+      >Sign In</button>
+    </li>
+  )
+}
